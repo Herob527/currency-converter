be21d1c9f55f76d86123aad54fa4e4a1be123f7a
Herob527 <szymon.wrzos@onet.pl>
2025-09-17 12:48:39 +0200

    Extract cubit to own var and add live conversion between currencies

--- lib/pages/main.dart
+++ lib/pages/main.dart
@@ -1,5 +1,6 @@
 import 'package:flutter/material.dart';
 import 'package:flutter_bloc/flutter_bloc.dart';
+import 'package:flutter_currency/constants.dart';
 import 'package:flutter_currency/models/currency.dart';
 
 class MyHomePage extends StatelessWidget {
@@ -7,6 +8,7 @@ class MyHomePage extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) {
+    final cubit = context.read<CurrencyCubit>();
     return Scaffold(
       body: BlocBuilder<CurrencyCubit, Currency>(
         builder: (context, currency) => Column(
@@ -14,7 +16,21 @@ class MyHomePage extends StatelessWidget {
           children: [
             Row(
               mainAxisAlignment: MainAxisAlignment.center,
-              children: <Widget>[Text(currency.toString())],
+              children: [Text(currency.toString())],
+            ),
+            Row(
+              mainAxisAlignment: MainAxisAlignment.center,
+              spacing: 16,
+              children: [
+                for (var val in conversionMapToPln.entries)
+                  Column(
+                    children: [
+                      Text(
+                        "${(currency.toDouble() / val.value).toStringAsFixed(2)} ${val.key}",
+                      ),
+                    ],
+                  ),
+              ],
             ),
           ],
         ),
@@ -27,14 +43,14 @@ class MyHomePage extends StatelessWidget {
           FloatingActionButton.small(
             child: const Icon(Icons.plus_one),
             onPressed: () {
-              context.read<CurrencyCubit>().increment();
+              cubit.increment();
             },
           ),
 
           FloatingActionButton.small(
             child: const Icon(Icons.exposure_minus_1),
             onPressed: () {
-              context.read<CurrencyCubit>().decrement();
+              cubit.decrement();
             },
           ),
         ],
