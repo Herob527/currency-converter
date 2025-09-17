e0340622ef83f61a9da8046bcd879c9cfbe609c6
Herob527 <szymon.wrzos@onet.pl>
2025-09-17 12:18:36 +0200

    Adjusted logic for incrementing

--- lib/models/currency.dart
+++ lib/models/currency.dart
@@ -1,5 +1,7 @@
 import 'package:flutter_bloc/flutter_bloc.dart';
 
+const defaultCurrencyCode = "PLN";
+
 class Currency {
   int big;
   int small;
@@ -21,8 +23,27 @@ class Currency {
 }
 
 class CurrencyCubit extends Cubit<Currency> {
-  CurrencyCubit() : super(Currency(small: 0, big: 0, code: ""));
+  CurrencyCubit()
+    : super(Currency(small: 0, big: 0, code: defaultCurrencyCode));
 
-  void increment() => emit(state.withCurrency(newSmall: state.small + 1));
-  void decrement() => emit(state.withCurrency(newSmall: state.small - 1));
+  void decrement() {
+    int newVal = state.small - 1;
+    if (state.big == 0 && newVal < 0) {
+      return;
+    }
+    if (state.big > 0 && newVal < 0) {
+      emit(state.withCurrency(newBig: state.big - 1, newSmall: 99));
+      return;
+    }
+    emit(state.withCurrency(newSmall: state.small - 1));
+  }
+
+  void increment() {
+    int newVal = state.small + 1;
+    if (newVal > 99) {
+      emit(state.withCurrency(newBig: state.big + 1, newSmall: 0));
+      return;
+    }
+    emit(state.withCurrency(newSmall: state.small + 1));
+  }
 }
