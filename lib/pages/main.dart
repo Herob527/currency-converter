b47b4589ce83ecc023dd8dca643a2a5e2ffa817d
Herob527 <szymon.wrzos@onet.pl>
2025-09-17 14:15:08 +0200

    Add flag to pl and use expanded to make equals

--- lib/pages/main.dart
+++ lib/pages/main.dart
@@ -1,4 +1,3 @@
-import 'package:country_flags/country_flags.dart';
 import 'package:flutter/material.dart';
 import 'package:flutter_bloc/flutter_bloc.dart';
 import 'package:flutter_currency/constants.dart';
@@ -13,28 +12,36 @@ class MyHomePage extends StatelessWidget {
     return Scaffold(
       body: BlocBuilder<CurrencyCubit, Currency>(
         builder: (context, currency) => Row(
-          mainAxisAlignment: MainAxisAlignment.center,
           children: [
-            Column(
-              mainAxisAlignment: MainAxisAlignment.center,
-              crossAxisAlignment: CrossAxisAlignment.center,
-              children: [Text(currency.toString())],
-            ),
-            Column(
-              mainAxisAlignment: MainAxisAlignment.center,
-              spacing: 16,
-              children: [
-                for (var val in conversionMapToPln.entries)
-                  Row(
-                    spacing: 8,
-                    children: [
-                      val.key.getFlag(),
-                      Text(
-                        "${(currency.toDouble() / val.value).toStringAsFixed(2)} ${val.key.currency}",
-                      ),
-                    ],
+            Expanded(
+              child: Column(
+                mainAxisAlignment: MainAxisAlignment.center,
+                children: [
+                  Country.poland.getFlag(),
+                  Text(
+                    currency.toString(),
+                    style: const TextStyle(fontSize: 32),
                   ),
-              ],
+                ],
+              ),
+            ),
+            Expanded(
+              child: Column(
+                mainAxisAlignment: MainAxisAlignment.start,
+                spacing: 16,
+                children: [
+                  for (var val in conversionMapToPln.entries)
+                    Row(
+                      spacing: 8,
+                      children: [
+                        val.key.getFlag(),
+                        Text(
+                          "${(currency.toDouble() / val.value).toStringAsFixed(2)} ${val.key.currency}",
+                        ),
+                      ],
+                    ),
+                ],
+              ),
             ),
           ],
         ),
