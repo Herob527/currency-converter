7498217dd8d41eda8570688e953cee563f1ed6ad
Herob527 <szymon.wrzos@onet.pl>
2025-09-17 13:19:34 +0200

    Added countries and switched map to use enum

--- lib/constants.dart
+++ lib/constants.dart
@@ -1,6 +1,29 @@
-const conversionMapToPln = <String, double>{
-  'USD': 3.64,
-  'EUR': 4.25,
-  'GBP': 3.56,
-  'INR': 3.25,
+import "package:country_flags/country_flags.dart";
+
+enum Country {
+  unitedStates(name: "United States", code: "US", currency: "USD"),
+  europeanUnion(name: "Germany", code: "DEU", currency: "EUR"),
+  unitedKingdom(name: "United Kingdom", code: "GB", currency: "GBP"),
+  india(name: "India", code: "IN", currency: "INR");
+
+  const Country({
+    required this.name,
+    required this.currency,
+    required this.code,
+  });
+
+  final String name;
+  final String code;
+  final String currency;
+
+  String get countryCurrency => currency;
+
+  getFlag() => CountryFlag.fromCountryCode(code);
+}
+
+const conversionMapToPln = <Country, double>{
+  Country.unitedStates: 3.64,
+  Country.europeanUnion: 4.25,
+  Country.unitedKingdom: 3.56,
+  Country.india: 3.25,
 };
