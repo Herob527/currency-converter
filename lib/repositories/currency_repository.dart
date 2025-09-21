29360fdeff9e2daf4482c50c3adf5cdbdca5223e
Herob527 <szymon.wrzos@onet.pl>
2025-09-21 20:13:17 +0200

    Adjustments

--- lib/repositories/currency_repository.dart
+++ lib/repositories/currency_repository.dart
@@ -3,13 +3,17 @@ import 'dart:convert';
 import 'package:flutter_currency/main.dart';
 import 'package:flutter_currency/models/currency_response.dart';
 
-const ApiUrl = "https://api.nbp.pl/api/exchangerates/tables/A?format=json";
+const apiUrl = "https://api.nbp.pl/api/exchangerates/tables/A?format=json";
 
 class CurrencyRepository {
-  getCurrencies() async {
-    var response = await dio.get<String>(ApiUrl);
-    var decoded = json.decode(response.data ?? "{}");
-    return response.data;
+  Future<CurrencyResponseModel?> getCurrencies() async {
+    var response = await dio.get<String>(apiUrl);
+    var data = response.data;
+    if (data == null) {
+      return Future.value(null);
+    }
+    var decoded = CurrencyResponseModel.fromJson(json.decode(data));
+    return decoded;
   }
 }
 
